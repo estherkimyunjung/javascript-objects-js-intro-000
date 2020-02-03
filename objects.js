c5eb11e70067962aafbe29c01019eaa78c50a9fc
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-03 15:34:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,6 @@
 var playlist = {artistName: "songTitle"};
-//function updatePlaylist(playlist, artistName, songTitle)
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  var wholePlaylist = Object.assign({}, playlist, {artistName: songTitle})
+  return wholePlaylist
+}
